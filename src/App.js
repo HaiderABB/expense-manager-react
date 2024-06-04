547e50f8ed93b242789abcc24f89b0b75f9ebb5b
HaiderABB <haider.a.moazzam@gmail.com>
2024-06-04 18:12:54 +0500

    Components Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
-
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpense from './components/IncomeExpense';
+import TransactionList from './components/TransactionList.jsx'
+import AddTransaction from './components/AddTransaction.jsx'
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div> 
+    <Header></Header>
+    <div className="container"></div>
+    <Balance/>
+    <div class="inc-exp-container">
+      <IncomeExpense></IncomeExpense>
+      </div>
+      <TransactionList></TransactionList>
+      <AddTransaction></AddTransaction>
     </div>
+
   );
 }
 
