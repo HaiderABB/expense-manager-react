3b7bfe1cd468b86f3e187eaf8d3a9e5e7160bd8b
HaiderABB <haider.a.moazzam@gmail.com>
2024-06-05 15:17:50 +0500

    Complete Project

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import './App.css';
 import Header from './components/Header';
 import Balance from './components/Balance';
 import IncomeExpense from './components/IncomeExpense';
-import TransactionList from './components/TransactionList.jsx'
-import AddTransaction from './components/AddTransaction.jsx'
+import TransactionList from './components/TransactionList.jsx';
+import AddTransaction from './components/AddTransaction.jsx';
+import { GlobalProvider } from './context/GlobalState';
 function App() {
   return (
-    <div> 
-    <Header></Header>
-    <div className="container"></div>
-    <Balance/>
-    <div class="inc-exp-container">
+    <GlobalProvider>
+      <Header></Header>
+      <div className="container"></div>
+      <Balance />
       <IncomeExpense></IncomeExpense>
-      </div>
       <TransactionList></TransactionList>
       <AddTransaction></AddTransaction>
-    </div>
-
+    </GlobalProvider>
   );
 }
 
